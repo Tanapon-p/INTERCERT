5c34ac9ec144e45bf3af46c0f09209668e609bd8
Christmas <christmas@rayoutdesign.com>
2024-08-26 21:02:58 +0700

    Install Google Tags

--- web/component/header.php
+++ web/component/header.php
@@ -1,3 +1,7 @@
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PDFL6WQN" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <?php require_once "./static/iso_menu.php" ?>
 
 
